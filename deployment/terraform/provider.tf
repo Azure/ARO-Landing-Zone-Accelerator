1a9d8531bddef1409b3e267e123722c46a6e670f
Jonathan Gardner <jgardner@fastmail.com>
2022-05-12 13:44:24 -0700

    Add provider and initial variables

---
+++ deployment/terraform/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = var.subscription_id
+  tenant_id = var.tenant_id
+  features {}
+}
---
+++ deployment/terraform/variables.tf
@@ -0,0 +1,9 @@
+# Variables
+
+variable "tenant_id" {
+  type = string
+}
+
+variable "subscription_id" {
+  type = string
+}
