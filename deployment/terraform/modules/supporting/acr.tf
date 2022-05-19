2e77c11c90c6895e509efea53586a5ffb8e1589c
Jonathan Gardner <jgardner@fastmail.com>
2022-05-19 14:15:42 -0700

    Refactor data sources to separate file

--- deployment/terraform/modules/supporting/acr.tf
+++ deployment/terraform/modules/supporting/acr.tf
@@ -1,14 +1,3 @@
-data "azurerm_virtual_network" "spoke_vnet" {
-  name = var.vnet_name
-  resource_group_name = var.spoke_rg_name
-}
-
-data "azurerm_subnet" "subnet" {
-  name = var.private_endpoint_subnet_name
-  virtual_network_name = data.azurerm_virtual_network.spoke_vnet.name
-  resource_group_name = var.spoke_rg_name
-}
-
 resource "random_string" "acr" {
   length = 4
   min_numeric = 4
@@ -20,7 +9,7 @@ resource "random_string" "acr" {
 resource "azurerm_container_registry" "acr" {
   name = "${var.base_name}${random_string.acr.result}"
   resource_group_name = var.spoke_rg_name
-  location = var.location
+  location = data.azurerm_resource_group.spoke.location
   sku = "Premium"
   admin_enabled = true
   public_network_access_enabled = false
@@ -29,8 +18,8 @@ resource "azurerm_container_registry" "acr" {
 resource "azurerm_private_endpoint" "acr" {
   name = "arcPvtEndpoint"
   resource_group_name = var.spoke_rg_name
-  location = var.location
-  subnet_id = data.azurerm_subnet.subnet.id
+  location = data.azurerm_resource_group.spoke.location
+  subnet_id = data.azurerm_subnet.private_endpoint_subnet_name.id
 
   private_dns_zone_group {
     name = "ACR-ZoneGroup"
