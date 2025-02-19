a58de910919766f7e74ebbd0c7293491af97ab65
Alain Vetier <alain.vetier@outlook.com>
2025-02-20 01:45:46 +0800

    spoke kv config update

--- Scenarios/Secure-Baseline/terraform/modules/supporting/sup_kv.tf
+++ Scenarios/Secure-Baseline/terraform/modules/supporting/sup_kv.tf
@@ -5,6 +5,7 @@ resource "azurerm_key_vault" "sub_kv" {
   tenant_id = data.azurerm_client_config.current.tenant_id
   sku_name = "standard"
   purge_protection_enabled = false
+  public_network_access_enabled = false
   network_acls {
     bypass = "AzureServices"
     default_action = "Deny"
